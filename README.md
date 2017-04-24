91449cf6529bf639daabebdac580dd9c1f8b0607
omcrest <om.bhakre@springernature.com>
2017-04-24 22:32:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CA-Office-Manager
+Sample Java Web Application for office management
---
+++ src/main/java/com/caofficemanager/web/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.caofficemanager.web.controller;
+
+/**
+ * Created by bhak01 on 18/04/17.
+ */
+public class TestController {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
